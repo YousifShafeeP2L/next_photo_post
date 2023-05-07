0fe7fae561a2147836b40ed590d57c860bcd9e65
YousifShafeeP2L <yousif@path2live.com>
2023-05-07 15:12:52 +0300

    empty upload photo page

---
+++ pages/photo/upload.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Upload() {
+    return (
+        <>
+            <Head>
+                <title>Upload Photo</title>
+            </Head>
+            <div style={{height:"500px"}}></div>
+        </>
+    )
+}
