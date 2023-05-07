d7557eb8d0d561ece800dea64cea5f3137115ab5
YousifShafeeP2L <yousif@path2live.com>
2023-05-07 15:14:40 +0300

    view photos in home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Photos from './photo/photos';
 
 export default function Home(props) {
   return (
@@ -6,6 +7,21 @@ export default function Home(props) {
       <Head>
         <title>Home</title>
       </Head>
+      <Photos images={props.images} status={"A"}></Photos>
     </>
   )
+}
+
+// Fetching data from the JSON file
+import fsPromises from 'fs/promises';
+import path from 'path'
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'public/data.json');
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData);
+
+  return {
+    props: objectData
+  }
 }
