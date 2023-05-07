18296a1c135e03ceb26a7840af43a85304b70b84
YousifShafeeP2L <yousif@path2live.com>
2023-05-07 15:12:15 +0300

    empty review photos page

---
+++ pages/photo/review.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Review() {
+    return (
+        <>
+            <Head>
+                <title>Review Photo</title>
+            </Head>
+            <div style={{height:"500px"}}></div>
+        </>
+    )
+}
