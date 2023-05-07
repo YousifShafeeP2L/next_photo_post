04eb77c8a9b909e411488c740c5d1eac5d10683e
YousifShafeeP2L <yousif@path2live.com>
2023-05-07 15:11:01 +0300

    footer component

---
+++ components/footer.js
@@ -0,0 +1,33 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="w3l-footer-29-main position-static">
+                <div className="footer-29 pt-5 pb-4">
+                    <div className="container pt-md-4">
+                        <div className="row ">
+                            <div className="col-lg-4 col-md-6 footer-list-29">
+                                <h6 className="footer-title-29">Contact Info </h6>
+                                <p className="mb-2 pe-xl-5">Address : Giza, Al-Haram, Pyramds Garden.
+                                </p>
+                                <p className="mb-2">Phone Number : <a href="tel:+20 1004836281">+20 1004836281</a></p>
+                                <p className="mb-2">Email : <a href="mailto:yousif@path2live.com">yousif@path2live.com</a></p>
+                            </div>
+                            <div className="col-lg-2 col-md-3 col-6 footer-list-29 mt-md-0 mt-4">
+                                <ul>
+                                    <h6 className="footer-title-29">Quick Links</h6>
+                                    <li><a href="/Home">Home</a></li>
+                                    <li><a href="/upload_photo">Review Photos</a></li>
+                                    <li><a href="/contact">Upload Photos</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-4 col-md-6 col-sm-8 footer-list-29 mt-lg-0 mt-4 ps-lg-5">
+                                <h6 className="footer-title-29">Subscribe</h6>
+                                <p className="mt-3">Subscribe to our mailing list and get updates to your email inbox.</p>
+                            </div>
+                        </div>
+                    </div>``
+                </div>
+            </footer>
+        </>
+    );
+}
